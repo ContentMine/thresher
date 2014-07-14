f6a895551f43eb70e926845976e2815e8029beb9
Richard Smith <rds45@cam.ac.uk>
2014-07-14 13:16:28 +0200

    fix download tests

--- test/downloadSpec.js
+++ test/downloadSpec.js
@@ -2,12 +2,17 @@ var dl = require('../lib/download.js');
 var fs = require('fs');
 var should = require('should');
 var temp = require('temp');
+temp.track();
+
+require('shelljs/global');
+
 log = {
   info: function(){},
   warn: function(){},
   error: function(){},
   debug: function(){}
 };
+
 var MOCKPORT = 30045
 var mockserver = require('./mockserver').app.listen(MOCKPORT);
 
@@ -16,14 +21,14 @@ describe("download", function() {
   describe("downloadResource()", function() {
 
     it("should handle relative URLs", function(done) {
-      temp.open('download', function(err, info) {
+      temp.mkdir('download', function(err, dirPath) {
+        cd(dirPath);
         var resUrl = '/data/tiny.html'
         var scrapeUrl = 'http://localhost:' + MOCKPORT
         var rename = null;
         var down = dl.downloadResource(resUrl, scrapeUrl, rename);
         down.on('close', function() {
           fs.existsSync('tiny.html').should.be.ok;
-          temp.cleanupSync();
           done();
         });
         down.on('error', function(err) {
@@ -38,7 +43,8 @@ describe("download", function() {
     });
 
     it("should handle absolute URLs", function(done) {
-      temp.open('download', function(err, info) {
+      temp.mkdir('download', function(err, dirPath) {
+        cd(dirPath);
         var resUrl = 'http://localhost:' + MOCKPORT + '/data/tiny2.html'
         var scrapeUrl = 'http://localhost:' + MOCKPORT
         var rename = null;
@@ -47,7 +53,6 @@ describe("download", function() {
           fs.existsSync('tiny2.html').should.be.ok;
           fs.unlink('tiny2.html', function (err) {
             if (err) throw err;
-            temp.cleanupSync();
             done();
           });
         });
@@ -63,14 +68,14 @@ describe("download", function() {
     });
 
     it("should rename downloaded files", function(done) {
-      temp.open('download', function(err, info) {
+      temp.mkdir('download', function(err, dirPath) {
+        cd(dirPath);
         var resUrl = 'data/tiny.html'
         var scrapeUrl = 'http://localhost:' + MOCKPORT
         var rename = 'blimey.html';
         var down = dl.downloadResource(resUrl, scrapeUrl, rename);
         down.on('close', function() {
           fs.existsSync('blimey.html').should.be.ok;
-          temp.cleanupSync();
           done();
         });
         down.on('error', function(err) {
